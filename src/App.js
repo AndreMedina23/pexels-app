d69d0b28220b52e9ba4e0898caa55fa7fd7085c3
AndreMedina23 <147629957+AndreMedina23@users.noreply.github.com>
2023-12-08 19:32:58 -0600

    Ptoyecto React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import PhotoList from './Components/PhotoList';
+import './App.css'; // Importa el archivo CSS
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PhotoList />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/Components/PhotoList.js
@@ -0,0 +1,51 @@
+// src/components/PhotoList.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PhotoList = () => {
+  const [photos, setPhotos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.pexels.com/v1/search?query=${searchTerm}`,
+          {
+            headers: {
+              Authorization: 'CBj46ptnDt36Mf8gOgdhqQ7s1z8BqIYzI2OshgSWvS8rLiuvGjM6kviG',
+            },
+          }
+        );
+        setPhotos(response.data.photos);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <h1>Buscador de Imágenes</h1>
+      <input
+        type="text"
+        placeholder="Buscar imágenes..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div>
+        {photos.map((photo) => (
+          <img
+            key={photo.id}
+            src={photo.src.medium}
+            alt={photo.photographer}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoList;
